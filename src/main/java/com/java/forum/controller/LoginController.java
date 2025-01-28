98bc0e7d296f138711186ca3ce4c33f818b3675f
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-01-28 18:48:35 +0800

    fix: "/kaptcha" in LoginController.java HttpResponse's package name changed since Springboot3

--- src/main/java/com/java/forum/controller/LoginController.java
+++ src/main/java/com/java/forum/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.java.forum.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.java.forum.entity.User;
 import com.java.forum.service.UserService;
 import com.java.forum.util.ForumConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements ForumConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage(){
@@ -59,5 +73,26 @@ public class LoginController implements ForumConstant {
 
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path="/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //generate captcha
+        String kaptchaText = kaptchaProducer.createText();
+        BufferedImage kaptchaImage = kaptchaProducer.createImage(kaptchaText);
+
+        //store the captcha in to session
+        session.setAttribute("kaptcha", kaptchaText);
+
+        //send the kaptchaImage to the browser
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(kaptchaImage,"png",os);
+        } catch (IOException e) {
+            logger.error("Cannot send the captchaImage:{}",e.getMessage());
+        }
+
+
     }
 }
