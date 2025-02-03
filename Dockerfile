7301590a8d532729ce763c93c2e869930f80baf0
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-02-03 16:22:25 +0800

    chore: Dockerfile don't need build jar(but it's slower)

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,17 @@
 FROM eclipse-temurin:17-jdk
 COPY forum-0.0.1-SNAPSHOT.jar app.jar
+
+
+#FROM maven:3.8.4-openjdk-17-slim as builder
+#WORKDIR /app
+#
+#COPY pom.xml .
+#COPY src ./src
+#
+#RUN mvn clean package -DskipTests
+#
+#FROM eclipse-temurin:17-jdk
+#
+#COPY --from=builder /app/target/forum-0.0.1-SNAPSHOT.jar app.jar
+
+
---
+++ src/main/java/com/java/forum/config/ThymeleafConfig.java
@@ -0,0 +1,28 @@
+package com.java.forum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+}
