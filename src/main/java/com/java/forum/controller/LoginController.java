2c10df40509992c27a67ce9ab6c27d1ad07a34b4
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-02-14 18:04:21 +0800

    refactor: captcha to redis

--- src/main/java/com/java/forum/controller/LoginController.java
+++ src/main/java/com/java/forum/controller/LoginController.java
@@ -4,12 +4,15 @@ import com.google.code.kaptcha.Producer;
 import com.java.forum.entity.User;
 import com.java.forum.service.UserService;
 import com.java.forum.util.ForumConstant;
+import com.java.forum.util.ForumUtil;
+import com.java.forum.util.RedisKeyUtil;
 import jakarta.servlet.http.Cookie;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -26,11 +29,15 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class LoginController implements ForumConstant {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
     private UserService userService;
@@ -85,13 +92,24 @@ public class LoginController implements ForumConstant {
     }
 
     @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
-    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+    public void getKaptcha(HttpServletResponse response/*, HttpSession session*/) {
         //generate captcha
         String kaptchaText = kaptchaProducer.createText();
         BufferedImage kaptchaImage = kaptchaProducer.createImage(kaptchaText);
 
         //store the captcha in to session
-        session.setAttribute("kaptcha", kaptchaText);
+        //session.setAttribute("kaptcha", kaptchaText);
+
+        //the owner of the kaptcha code is the random string
+        String kaptchaOwner = ForumUtil.generateUUID();
+        Cookie cookie = new Cookie("kaptchaOwner", kaptchaOwner);
+        cookie.setMaxAge(60);
+        cookie.setPath(contextPath);
+        response.addCookie(cookie);
+
+        //store the captcha in redis
+        String kaptchaKey = RedisKeyUtil.getKaptchaKey(kaptchaOwner);
+        redisTemplate.opsForValue().set(kaptchaKey, kaptchaText, 60, TimeUnit.SECONDS);
 
         //send the kaptchaImage to the browser
         response.setContentType("image/png");
@@ -107,9 +125,16 @@ public class LoginController implements ForumConstant {
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(String username, String password, String code, boolean rememberMe,
-                        Model model, HttpSession session, HttpServletResponse response) {
+                        Model model, /*HttpSession session,*/ HttpServletResponse response,
+                        @CookieValue("kaptchaOwner") String kaptchaOwner) {
         //verify the kaptcha
-        String kaptcha = (String) session.getAttribute("kaptcha");
+        //String kaptcha = (String) session.getAttribute("kaptcha");
+        String kaptcha = null;
+        if (StringUtils.isNotBlank(kaptchaOwner)) {
+            String kaptchaKey = RedisKeyUtil.getKaptchaKey(kaptchaOwner);
+            kaptcha = (String) redisTemplate.opsForValue().get(kaptchaKey);
+        }
+
         if (StringUtils.isBlank(kaptcha) || StringUtils.isBlank(code) || !kaptcha.equalsIgnoreCase(code)) {
             model.addAttribute("codeMsg", "Captcha error");
             return "site/login";
--- src/main/java/com/java/forum/util/RedisKeyUtil.java
+++ src/main/java/com/java/forum/util/RedisKeyUtil.java
@@ -4,14 +4,24 @@ public class RedisKeyUtil {
 
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+    private static final String PREFIX_TICKET = "ticket";
 
     // A key to store the like count of an entity
     // ex like:entity:1:2
-    public static String getEntityLikeKey(int entityType, int entityId){
+    public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
 
+    // A key to store the kaptcha code
+    //owner: the owner of the kaptcha code(random string)
+    public static String getKaptchaKey(String owner) {
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
 
+    public static String getTicketKey(String ticket) {
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
 
 
 }
